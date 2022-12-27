1a9e5cdee32e88bfda9240ee2905761e03ed5c3b
Odonno <bottiau.david@laposte.net>
2022-12-27 15:04:52 +0100

    ⬆️ upgrade tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,15 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: "jit",
-  purge: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false,
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       zIndex: {
         "-1": "-1",
       },
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [],
 };
